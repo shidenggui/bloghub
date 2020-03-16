d696ca2dfb7ca398cd680e771f5e44572a3136aa
shidenggui <903618848@qq.com>
2020-03-16 19:57:59 +0800

    :star:(rss) add chrome user-agent

--- backend/src/domains/infrastructure/rss-parser.ts
+++ backend/src/domains/infrastructure/rss-parser.ts
@@ -20,6 +20,7 @@ export class RssParser {
     },
     headers: {
       Accept: 'application/rss+xml, application/xml',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
     },
   })) {
   }
