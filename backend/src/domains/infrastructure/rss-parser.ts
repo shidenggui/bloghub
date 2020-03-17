c10e1471437e208e6617afaafb541007541a6d35
shidenggui <903618848@qq.com>
2020-03-17 09:10:06 +0800

    :star:(rss) allow extrace category  for https://beyondstars.xyz/posts/index.xml

--- backend/src/domains/infrastructure/rss-parser.ts
+++ backend/src/domains/infrastructure/rss-parser.ts
@@ -58,8 +58,13 @@ export class RssParser {
 
   private parseCategory(article: Parser.Item): string[] {
     let categories = article.categories?.filter(t => typeof t === 'string') || []
-    if (!categories.length && article.category?.$?.term)
-      categories = [article.category?.$.term]
+    if (!categories.length ) {
+      // for https://beyondstars.xyz/posts/index.xml
+      if (typeof article.category === 'string') categories = [article.category]
+      // for <category term='tag'/>
+      if (article.category?.$?.term) categories = [article.category.$.term]
+    }
+
     return categories
   }
 
